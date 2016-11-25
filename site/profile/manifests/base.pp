917b9a42c734f75285cfd049b2ea252753224b07
Brad Chamberlain <chamby@gmail.com>
2016-11-25 17:26:49 -0500

    added emergency account

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -10,4 +10,17 @@ class profile::base {
     locked     => true,
   }
 
+  accounts::user { 'emergency':
+    ensure => present,
+    uid => '460',
+    gid => '460',
+    shell => '/bin/bash',
+    password => '!!', # No password
+    sshkeys => [
+      "ssh-rsa AAAA...",
+      "ssh-rsa BBBB...",
+      "ssh-rsa CCCC...",
+    ],
+  }
+
 }
