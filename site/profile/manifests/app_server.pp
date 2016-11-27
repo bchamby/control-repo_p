b07a97d4a681e032776863098f415ed1fba9a3e5
Brad Chamberlain <chamby@gmail.com>
2016-11-27 09:02:47 -0500

    added registry key resource

--- site/profile/manifests/app_server.pp
+++ site/profile/manifests/app_server.pp
@@ -1,12 +1,17 @@
 class profile::app_server {
 
-  # if $facts['os']['family'] == 'windows' {
-  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
-    ensure => present,
-    data   => '0',
-    type   => 'dword',
+  if $facts['os']['family'] == 'windows' {
+
+    registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\':
+      ensure => present,
+    }
+    
+    registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
+      ensure => present,
+      data   => '0',
+      type   => 'dword',
+    }
   }
-  # }
 
   file { 'c:\temp\test.txt':
     content => 'This is the content of my test file!'
