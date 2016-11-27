db702332f263592441bbc311c303abd6b83e2bfd
Brad Chamberlain <chamby@gmail.com>
2016-11-27 08:34:23 -0500

    app server

--- site/profile/manifests/app_server.pp
+++ site/profile/manifests/app_server.pp
@@ -1,10 +1,10 @@
 class profile::app_server {
 
-  # if $facts['os']['family'] == 'windows' {
-  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
-    ensure => present,
-    data   => '0',
-    type   => 'dword',
+  if $facts['os']['family'] == 'windows' {
+    registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
+      ensure => present,
+      data   => '0',
+      type   => 'dword',
+    }
   }
-  # }
 }
