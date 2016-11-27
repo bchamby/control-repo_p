972b48c9aecfc017b37b0bcf4215cd357d3d1b81
Brad Chamberlain <chamby@gmail.com>
2016-11-27 09:04:43 -0500

    testing without os family logic

--- site/profile/manifests/app_server.pp
+++ site/profile/manifests/app_server.pp
@@ -2,10 +2,10 @@ class profile::app_server {
 
   if $facts['os']['family'] == 'windows' {
 
-    registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\':
-      ensure => present,
-    }
-    
+    # registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
+    #   ensure => present,
+    # }
+
     registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
       ensure => present,
       data   => '0',
